6927a6bab90f96dac84427e512cc0fc124886b59
core.pager=cat <yacinebabour76@gmail.com>
2022-03-09 20:26:12 +0100

    first commit

---
+++ src/component/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div class="card bg-dark text-white border-0">
+        <img src="/assets/2.jpg" class="card-img" alt=">Background" height="550px" />
+        <div class="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="coantainer">
+          <h5 class="card-title display-3 fw-bolder mb-0">New Arrrival</h5>
+          <p class="card-text lead fs-2">
+            CHECK OUT THE TRENDS
+          </p>
+          </div>
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
